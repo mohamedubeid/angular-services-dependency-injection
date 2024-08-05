ec939892242a1711d339e3cdcfd791e8c612c8a1
MohamedUbeid9 <mubaid@vast.ps>
2024-08-05 13:08:54 +0300

    setup project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
-import { NgModule } from '@angular/core';
+import { NgModule, InjectionToken } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './header/home/home.component';
+import { AdminComponent } from './header/admin/admin.component';
+import { HeroComponent } from './header/home/hero/hero.component';
+import { SidebarComponent } from './header/home/sidebar/sidebar.component';
+import { UserListComponent } from './header/admin/user-list/user-list.component';
+import { FormsModule } from '@angular/forms';
+import { UserDetailComponent } from './header/admin/user-detail/user-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    AdminComponent,
+    HeroComponent,
+    SidebarComponent,
+    UserListComponent,
+    UserDetailComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
